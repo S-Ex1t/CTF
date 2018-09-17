4a60a53b62980bae1ff94eddb076be0a1dc1a3bb
scai16 <42941558+scai16@users.noreply.github.com>
2018-09-17 17:15:10 -0400

    Update solve.py

--- 2018/CSAW CTF 2018/whyOS/solve.py
+++ 2018/CSAW CTF 2018/whyOS/solve.py
@@ -1,11 +1,10 @@
 import re
-with open('console.log','r') as f:
+with open('console.log', 'r') as f:
     data = [i.strip() for i in f]
 
 matches = []
 for i in data:
-    tmp = i.split(' ')
-    if re.match('^[a-f0-9]{32,}$',tmp[-1],re.I):
+    if re.match('^[a-f0-9]{32,}$', i.split()[-1], re.I):
             matches.append(i)
 
 #for i in matches:
