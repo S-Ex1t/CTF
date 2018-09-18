b12506f7dc4fa32dd7d21703911d0d180ec240a6
scai16 <42941558+scai16@users.noreply.github.com>
2018-09-18 18:46:02 -0400

    Update solve.py

--- 2018/CSAW CTF 2018/whyOS/solve.py
+++ 2018/CSAW CTF 2018/whyOS/solve.py
@@ -1,12 +1,12 @@
 import re
-with open('console.log','r') as f:
+with open('console.log', 'r') as f:
     data = [i for i in f]
     
 matches = []
 for i in data:
     if i.isspace():
         continue
-    if re.match('^[a-f0-9]{32,}$',i.split()[-1],re.I):
+    if re.match('^[a-f0-9]{32,}$', i.split()[-1], re.I):
             matches.append(i)
 
 #for i in matches:
